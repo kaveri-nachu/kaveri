1c7b9799617735f5fba350d71edfa576126d90ba
kaveri <kaveri.nachammai@gmail.com>
2017-04-18 15:38:15 +0530

    Update count.c

--- count.c
+++ count.c
@@ -8,7 +8,7 @@ scanf("%d",&digit1,&digit2,&digit3,&digit3,&digit4);
 digit1=(digit1%1000)/10;
 digit2=(digit2%1000)/10;
 digit3=(digit3%1000)/10;
-digit4=(digit4%1000)/10
+digit4=(digit4%1000)/10;
 sum=digit1+digit2+digit3+digit4;
 printf("answer is:%d",sum);
 return(0);
