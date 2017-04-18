0be67d8655f521bef6264209c435a0ff0e22f1bb
kaveri <kaveri.nachammai@gmail.com>
2017-04-18 14:53:06 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int i,n;
+char ch;
+printf("\n enter the number: ");
+scanf("%d",&n);
+for(i=0;i<5;i++)
+{
+printf("\n Hello");
+}getch();
+return 0;
+}
