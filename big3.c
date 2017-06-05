3ef5fb9601589e1200668b31ed3d5de6937647ae
kaveri <kaveri.nachammai@gmail.com>
2017-06-05 10:57:29 +0530

    Create big3.c

---
+++ big3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,l;
+printf("entrr three numbers:");
+scanf("%d,%d,%d",&n1,&n2,&n3);
+if(a>b)
+{
+if(a>c)
+printf("%d",l=a,n1);
+else
+printf("%d",l=a,n3);
+}
+else
+{
+if(n2>=n3)
+printf("%d",l=b,n2);
+else
+printf("largest value=%d"\n,l);
+}
+
