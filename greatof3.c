c0ea30708f4d602d8bc590d393b643216e0547d5
kaveri <kaveri.nachammai@gmail.com>
2017-06-05 12:24:47 +0530

    Create greatof3.c

---
+++ greatof3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,l;
+printf("enter three numbers:");
+scanf("%d,%d,%d",&n1,&n2,&n3);
+if(a>b)
+{
+if(a>c)
+printf("%d,l=a",a);
+else
+printf("%d,l=a",c);
+}
+else
+{
+if(b>c)
+printf("%d,l=b",b);
+else
+printf("largest value=%d",/n);
+}
+}
