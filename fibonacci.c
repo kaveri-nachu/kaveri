8e0567e6babde489b6e5ad151e85610b49cf79d8
kaveri <kaveri.nachammai@gmail.com>
2017-06-05 11:53:54 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int i,n,t1=0,t2=1,next term=0;
+printf("enter the number of terms:");
+scanf("%d"'&n);
+printf("fibonacci series:");
+for(i=1;i<=n;++i)
+{
+if(i==1)
+{
+printf("%d",t1);
+}
+continue;
+if(i==2)
+{
+printf("%d",t2);
+continue;
+}
+next trem=t1+t2;
+t1=t2;
+t2=next term;
+printf("%d",next term);
+}
+}
