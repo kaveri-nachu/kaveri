818f0535aa43c3f6e8b42a6a642fbcad84e1d808
kaveri <kaveri.nachammai@gmail.com>
2017-06-05 15:16:17 +0530

    Create even2.c

---
+++ even2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int counter;
+printf("even numbers betweeen 1 to 100/n");
+for(counter=1;counter<=100;counter++)
+{
+if (counter%2==0)
+{
+printf("%d",counter);
+}
+}
+}
