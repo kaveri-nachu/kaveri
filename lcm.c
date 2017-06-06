5d3283c459bccaf074afbf62218c44df09be24e9
kaveri <kaveri.nachammai@gmail.com>
2017-06-06 14:47:58 +0530

    Create lcm.c

---
+++ lcm.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,minMultiple;
+printf("enter two positive integers:");
+scanf("%d,%d",&n1,&n2);
+minMultiple=(n1>n2)?n1:n2;
+while(1)
+{
+if(minMultiple%n1==0 && minMultiple%n2==0)
+{
+printf("the lcm of %d and %d is %d",n1,n2,minMultiple);
+break;
+}
+++ minMultiple;
+}
+}
