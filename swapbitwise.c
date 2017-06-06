ed7647ba00138747bba5bb9abc0b0deab71150b5
kaveri <kaveri.nachammai@gmail.com>
2017-06-06 21:01:24 +0530

    Create swapbitwise.c

---
+++ swapbitwise.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int i=65;
+int k=120;
+printf("value of i=%d k=%d before swapping",i,k);
+i=i^k;
+k=i^k;
+i=i^k;
+printf("value of i=%d k=%d after swapping",i,k);
+}
