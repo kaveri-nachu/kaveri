aa9486b08ccd921bf3e40c040a7f7ab0710188ca
kaveri <kaveri.nachammai@gmail.com>
2017-06-06 16:00:20 +0530

    Create sumless.java

---
+++ sumless.java
@@ -0,0 +1,13 @@
+class sum
+{
+public static void main(String args[])
+int sum,n,temp;
+sum=0;
+n=100;
+while(n<=200)
+{
+sum=sum+n;
+}
+n++;
+System.out.println("sum of all numbers less than given numbers is:"+sum);
+}
