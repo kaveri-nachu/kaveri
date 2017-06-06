ef3d4f8a306ee48c8c6d3a7ae06c883bed9b7c67
kaveri <kaveri.nachammai@gmail.com>
2017-06-06 20:20:22 +0530

    Create sumfrom1545.c

---
+++ sumfrom1545.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+int i,k,sum=0;
+{
+printf("sum of all numbers from 1 to 15\n");
+scanf("%d",sum);
+for(i=1;i<=15;i++)
+{
+sum+=i;
+}
+printf("sum=%d",sum);
+}
+for(k=15;k<=45;k++)
+{
+if(k%2==1)
+}
+printf("sum of all odd numbers feom 15 to 45 \n");
+scanf("%d",sum);
+{
+sum=sum+k;
+}
+printf("sum of all odd numbers between %d to %d",sum);
