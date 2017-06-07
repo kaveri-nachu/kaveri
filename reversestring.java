d68b92723e385368929403e1aaf7d82c43820df7
kaveri <kaveri.nachammai@gmail.com>
2017-06-07 10:44:31 +0530

    Create reversestring.java

---
+++ reversestring.java
@@ -0,0 +1,9 @@
+public class ReverseString
+{
+public static void main(String[] args)
+    {
+        String str="MyJava";
+ StringBuffer sbf = new StringBuffer(str);
+ }
+ System.out.println(sbf.reverse());
+ }
