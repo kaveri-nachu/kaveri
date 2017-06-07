86d2397c481714632aa9e6e17307a112a7662294
kaveri <kaveri.nachammai@gmail.com>
2017-06-07 15:17:46 +0530

    Create onelinecfnpwr.c

---
+++ onelinecfnpwr.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define bool int
+ bool isPowerOfTwo(int n)
+{
+  if (n == 0)
+    return 0;
+    }
+  while (n != 1)
+  {
+    if (n%2 != 0)
+    {
+      return 0;
+    n = n/2;
+  }
+  return 1;
+}
+ 
