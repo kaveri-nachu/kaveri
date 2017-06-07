e595b099ca8a393537be4215a2be603139284ec6
kaveri <kaveri.nachammai@gmail.com>
2017-06-07 11:16:28 +0530

    Create revrsedigits.c

---
+++ revrsedigits.c
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class ReverseNumberWhile
+{
+   public static void main(String args[])
+   {
+      int num=0;
+      int reversenum =0;
+      System.out.println("Input your number and press enter: ");
+      Scanner in = new Scanner(System.in);
+      num = in.nextInt();
+      while( num != 0 )
+      {
+          reversenum = reversenum * 10;
+          reversenum = reversenum + num%10;
+          num = num/10;
+      }
+
+      System.out.println("Reverse of input number is: "+reversenum);
+   }
+}
