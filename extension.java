042170d5f4721f5487ece67c098ef50b59d6863d
kaveri <kaveri.nachammai@gmail.com>
2017-06-14 22:27:37 +0530

    Create extension.java

---
+++ extension.java
@@ -0,0 +1,69 @@
+public class Find {
+
+    public static class Finder
+        extends SimpleFileVisitor<Path> {
+
+        private final PathMatcher matcher;
+        private int numMatches = 0;
+
+        Finder(String pattern) {
+            matcher = FileSystems.getDefault()
+                    .getPathMatcher("glob:" + pattern);
+        }
+
+         void find(Path file) {
+            Path name = file.getFileName();
+            if (name != null && matcher.matches(name)) {
+                numMatches++;
+                System.out.println(file);
+            }
+        }
+
+       
+        void done() {
+            System.out.println("Matched: "
+                + numMatches);
+        }
+
+        
+        public FileVisitResult visitFile(Path file,
+                BasicFileAttributes attrs) {
+            find(file);
+            return CONTINUE;
+        }
+
+       
+        public FileVisitResult preVisitDirectory(Path dir,
+                BasicFileAttributes attrs) {
+            find(dir);
+            return CONTINUE;
+        }
+
+       
+        public FileVisitResult visitFileFailed(Path file,
+                IOException exc) {
+            System.err.println(exc);
+            return CONTINUE;
+        }
+    }
+
+    static void usage() {
+        System.err.println("java Find <path>" +
+            " -name \"<glob_pattern>\"");
+        System.exit(-1);
+    }
+
+    public static void main(String[] args)
+        throws IOException {
+
+        if (args.length < 3 || !args[1].equals("-name"))
+            usage();
+
+        Path startingDir = Paths.get(args[0]);
+        String pattern = args[2];
+
+        Finder finder = new Finder(pattern);
+        Files.walkFileTree(startingDir, finder);
+        finder.done();
+    }
+}
