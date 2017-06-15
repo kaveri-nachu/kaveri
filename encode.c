7c738016a3ed0aa77fd5b3d9bb7e5588d2be81f8
kaveri <kaveri.nachammai@gmail.com>
2017-06-15 12:31:19 +0530

    Create encode.c

---
+++ encode.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ char* encode(char* str) 
+ {
+   int i = 0;
+  while (str[i] != '\0')
+  {
+      str[i] = str[i] - 30;  
+      i++;
+   }
+   return (str);
+}
+ void main()
+ {
+ 
+   char *str;
+ 
+   printf("\nEnter the String to be Encode : ");
+   gets(str);
+ 
+   str = encode(str);
+   printf("\nEncoded String : %s", str);
+ }
