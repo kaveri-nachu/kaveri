da1f3a55d68dfe29f37290e51eb4004cc5376b87
kaveri <kaveri.nachammai@gmail.com>
2017-06-20 20:42:35 +0530

    Create uniquearr.c

---
+++ uniquearr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ int main()
+{
+    int arr[] = {3, 3, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("The element with single occurrence is %d ",
+            getSingle(arr, n));
+    return 0;
+}
+int getSingle(int arr[], int n)
+{
+    int ones = 0, twos = 0 ;
+  int common_bit_mask;
+for( int i=0; i< n; i++ )
+    {
+       twos  = twos | (ones & arr[i]);
+       ones  = ones ^ arr[i];
+ common_bit_mask = ~(ones & twos);
+         ones &= common_bit_mask;
+ twos &= common_bit_mask;
+ }
+  return ones;
+}
+ 
