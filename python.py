a047f61401fdef4906000c3c3ff8774246bef606
kaveri <kaveri.nachammai@gmail.com>
2017-06-28 11:08:24 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,8 @@
+Creating an educational project
+First Program and Comments
+Executing a task
+Navigating between tasks
+Starting a task all over again
+Getting task hints
+Navigating between answer placeholders
+Using Python console
