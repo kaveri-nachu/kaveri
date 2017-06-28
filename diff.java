aac3ba50d5583ef59308a0fc449015872db3b82c
kaveri <kaveri.nachammai@gmail.com>
2017-06-28 12:02:07 +0530

    Create diff.java

---
+++ diff.java
@@ -0,0 +1,15 @@
+public class Diff
+{
+public static void main(String args[])
+{
+	int a[]=new int [] {2,4,1,6,4,2,2,9,7};
+	for(int i=0;i<=a.length;i++)
+	{
+			
+		if(i>0)
+		{
+						}
+			System.out.println(a[i]-a[i+1]); 
+}
+	}
+}
