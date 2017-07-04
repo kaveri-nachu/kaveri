a5e0dcbcf34fcd1c1703243daa1bf360d53c107e
kaveri <kaveri.nachammai@gmail.com>
2017-07-04 12:14:46 +0530

    Create random.c

---
+++ random.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+  int r,a,b;
+  puts("100 Random Numbers");
+  for(a=0;a<20;a++)
+ {
+   for(b=0;b<5;b++)
+ {
+  s=rand();
+  printf("%dt",s);
+ }
+ putchar('n');
+ }
+}
