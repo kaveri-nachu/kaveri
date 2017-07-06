4fcf5c3381e759f6d6ccb9eb447a15ce588b6330
kaveri <kaveri.nachammai@gmail.com>
2017-07-06 12:40:50 +0530

    Create numtostr.java

---
+++ numtostr.java
@@ -0,0 +1,33 @@
+public class Trans
+{
+public static void main(String[]args)
+{
+  Scanner s=new Scanner(System.in);
+  System.out.println("Enter the values");
+  int i=s.nextInt();
+  switch(i)
+{
+case 1:
+System.out.println("a");
+break;
+case 2:
+System.out.println("b");
+break;
+case 3:
+System.out.println("c");
+break;
+case 4:
+System.out.println("d");
+break;
+case 5:
+System.out.println("e");
+break;
+case 12258:
+System.out.println("abbeh");
+break;
+default:
+System.out.println("invalid");
+break;
+}
+}
+}
