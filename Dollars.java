65bbc8b18ac30fdba6b9ab6b9dd515ba4870fadd
kaveri <kaveri.nachammai@gmail.com>
2017-07-11 20:33:31 +0530

    Create Dollars.java

---
+++ Dollars.java
@@ -0,0 +1,18 @@
+public class Dollar
+{
+    public static void main(String[]args, int i)
+    {
+      String s1="hello";
+     for( i=0;i<=s1.length();i++)
+{
+    char e=s1.charAt(i);
+    if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U'))
+    {
+        {
+String h=s1.substring(0, i);
+String g=s1.substring(i+ i);
+s1=h+"Z"+g;
+   }}
+}
+    System.out.println("The result is="+s1);
+      }}
