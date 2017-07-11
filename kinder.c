bdfbdda442d4386bbe3e8abaff4e75322adaa03f
kaveri <kaveri.nachammai@gmail.com>
2017-07-11 10:19:41 +0530

    Create kinder.c

---
+++ kinder.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e;
+printf("Enter the calculation:");
+scanf("%d",&a);
+printf("Enter the n value");
+scanf("%d",&b);
+printf("Enter the pair:");
+scanf("%d",&c);
+for(d=0;d<2*c;d++);
+{
+for(e=1;e=2*b-a;e++)
+if(c!=e)
+{
+printf("Enter the pair",c);
+printf("Enter the for day",e++,c);
+}
+}
+printf("Enter the calculation",a);
+}
