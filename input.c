55ef52eeb9f266a64ad926307613b28310098a6d
kaveri <kaveri.nachammai@gmail.com>
2017-07-11 15:34:04 +0530

    Update input.c

--- input.c
+++ input.c
@@ -1,14 +1,14 @@
-FILE1='somefile'
-FILE2='someotherfile'
-# File 1, column 1
+FILEa='somefile'
+FILEb='someotherfile'
+# File a, column 1
 f1c1=($(cut -f1 -s $FILE1))
-# File 1, column 2
+# File a, column 2
 #f1c2=($(cut -f2 -s $FILE1))
-  # File 2, column 1
+  # File b, column 1
 f2c1=($(cut -f1 -s $FILE2))
-# File 2, column 2
+# File b, column 2
 #f2c2=($(cut -f2 -s $FILE2))
-# Looping through all items in file 1 column 1
+# Looping through all items in file a column 1
 for x in "${f1c1[@]}"
 do
   # For each item in f1c1, check all items in f2c1 for a match
