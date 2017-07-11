8a7f55279e742fe77d1cb5afec1300b98193d9a0
kaveri <kaveri.nachammai@gmail.com>
2017-07-11 10:13:59 +0530

    Create Double.c

---
+++ Double.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  double temp,temp1;
+int i,count=0, median=0,log2;
+clrscr();
+scanf("%lf",&temp);
+temp1=1<<log2;
+ {
+count++;
+}
+}
+median = pow(2,(1<<(count/2)));
+printf("median:%f",median);
+}
