ea1b6db08578813cd5523d01092318c3e5792ca1
kaveri <kaveri.nachammai@gmail.com>
2017-07-13 22:23:12 +0530

    Create Robot.c

---
+++ Robot.c
@@ -0,0 +1,22 @@
+include<stdio.h>
+int main()
+{
+ int i,n,x,y,c,d;
+ printf("A robot have 2 type of commands\n");
+ scanf("%d",&n);
+ for(i=0;i<=x;i++)
+{
+ if(x>=0 && x<=10000)
+{
+ printf("command1:move forward%d",x);
+}
+}
+  for(i=0;i<=y;i++)
+{
+ if(y==-180 && y==180)
+{
+  printf("command2:Rotate by X degree:%d",y);
+}
+}
+ return 0;
+}
