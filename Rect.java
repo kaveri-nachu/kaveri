8011de56a8e8c1cc8658d8e3743febbd3c2b7a05
kaveri <kaveri.nachammai@gmail.com>
2017-07-13 22:32:47 +0530

    Create Rect.java

---
+++ Rect.java
@@ -0,0 +1,11 @@
+public class Rectangle
+{
+	   public RectangleHeigth(int i, int j, int k, int l)
+     {
+	   }
+   public static void main(String[]args)
+  {
+		   RectangleHeigth box= new RectangleHeigth(5,10,20,30);
+	        System.out.println(box);
+	    } 
+	}
